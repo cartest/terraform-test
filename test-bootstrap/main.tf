60137a703cfffc7de4d9be537c86617abc3241de
Mike Peachey <mike.peachey@bjss.com>
2016-08-18 14:02:03 +0100

    stash

--- test-bootstrap/main.tf
+++ test-bootstrap/main.tf
@@ -1,16 +1,31 @@
-module "vpc_test-1" {
-  source              = "../../terraform-bootstrap"
-  environment         = "env1"
-  project             = "project"
-  vpc_cidr            = "10.0.0.0/16"
-  private_dns_domain  = "env1.project.aws.test"
+variable "environment" {
+  type        = "string"
+  description = "Environment Name"
+  default     = "testenv"
 }
 
-module "vpc_test-2" {
-  source              = "../../terraform-bootstrap"
-  environment         = "env2"
-  project             = "project"
-  vpc_cidr            = "172.16.0.0/16"
-  private_dns_domain  = "env2.project.aws.test"
-  peer_matrix         = ["012345678901,vpc-00000000,10.0.0.0/16"] # would like to have vars here
+variable "project" {
+  type        = "string"
+  description = "Project Name"
+  default     = "testproj"
+}
+
+variable "vpc_cidr" {
+  type        = "string"
+  description = "VPC CIDR Block"
+  default     = "10.0.0.0/16"
+}
+
+variable "private_dns_domain" {
+  type        = "string"
+  description = "Private Hosted Zone DNS Domain Name"
+  default     = "testenv.testproj.aws.test"
+}
+
+module "bootstrap_test" {
+  source             = "../../terraform-bootstrap"
+  environment        = "${var.environment}"
+  project            = "${var.project}"
+  vpc_cidr           = "${var.vpc_cidr}"
+  private_dns_domain = "$[var.private_dns_domain}"
 }
--- test-environment/main.tf
+++ test-environment/main.tf
@@ -1,21 +1,10 @@
 module "env_test-1" {
   source                          = "../../terraform-environment"
-  environment                     = "${module.vpc_test-1.environment}"
-  project                         = "${module.vpc_test-1.project}"
-  aws_region                      = "${module.vpc_test-1.aws_region}"
-  vpc_id                          = "${module.vpc_test-1.vpc_id}"
-  bootstrap_public_route_table_id = "rtb-00000000"  # didn't work with variable passwed from boostrap module
-  internet_gateway_id             = "${module.vpc_test-1.internet_gateway_id}"
-  #example_bucket_name             = "shakalaka"  # optional
-}
-
-module "env_test-2" {
-  source                          = "../../terraform-environment"
-  environment                     = "${module.vpc_test-2.environment}"
-  project                         = "${module.vpc_test-2.project}"
-  aws_region                      = "${module.vpc_test-2.aws_region}"
-  vpc_id                          = "${module.vpc_test-2.vpc_id}"
-  bootstrap_public_route_table_id = "rtb-e50d7281"
-  internet_gateway_id             = "${module.vpc_test-2.internet_gateway_id}"
-  #example_bucket_name             = "shakalaka"  # optional
+  environment                     = "testenv"
+  project                         = "testproj"
+  aws_region                      = "eu-west-1"
+  vpc_id                          = "vpc-00000000000000000"
+  bootstrap_public_route_table_id = "rtb-00000000000000000"
+  internet_gateway_id             = "igw-00000000000000000"
+  example_bucket_name             = "iamabucket"
 }
