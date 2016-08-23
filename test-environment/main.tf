c53e6a8c833fcc7806a1eb00a616e945cd9d1dbc
Sebastian Mincewicz <s.mincewicz@kainos.com>
2016-08-23 18:23:04 +0100

    change things to make tests working

--- test-environment/main.tf
+++ test-environment/main.tf
@@ -3,8 +3,6 @@ module "env_test-1" {
   environment                     = "testenv"
   project                         = "testproj"
   aws_region                      = "eu-west-1"
-  vpc_id                          = "vpc-00000000000000000"
-  bootstrap_public_route_table_id = "rtb-00000000000000000"
-  internet_gateway_id             = "igw-00000000000000000"
-  example_bucket_name             = "iamabucket"
+  vpc_id                          = "vpc-XXXXXXXX"       # manual entry
+  #bucket_name                     = "iamabucket"        # optional entry
 }
--- test-microservice/main.tf
+++ test-microservice/main.tf
@@ -1,20 +1,75 @@
+provider "aws" {
+  region = "eu-west-1"
+}
+
+variable "private_nat_route_table_ids" {                          # just to simplify things for testing purposes
+  default = ["rtb-XXXXXXXX","rtb-XXXXXXXX","rtb-XXXXXXXX"]        # manual entries
+}
+
 module "mservice_test-1" {
   source                          = "../../terraform-microservice"
-  additional_security_group_ids   = ["${module.env_test-1.core_sg_id}"]
-  asg_size_max                    = "3"
-  asg_size_min                    = "3"
-  availability_zones              = "${module.env_test-1.azs_available_names}"
-  aws_region                      = "${module.env_test-1.aws_region}"
-  lc_ami_id                       = "ami-edb9069e"
+  name                            = "test-1"
+  vpc_id                          = "vpc-XXXXXXXX"      # manual entry
+  asg_size_min                    = "1"
+  asg_size_max                    = "2"
+  availability_zones              = ["eu-west-1a","eu-west-1b","eu-west-1c"]
+  aws_region                      = "eu-west-1"
+  lc_ami_id                       = "ami-XXXXXXXX"      # manual entry
   lc_instance_type                = "t2.micro"
-  name                            = "test1"
-  subnets_cidr                    = ["10.0.10.0/24", "10.0.20.0/24", "10.0.30.0/24"]
+  lc_key_name                     = "XXXXXXXX"          # manual entry
+  subnets_cidr                    = ["10.222.10.0/24", "10.222.20.0/24", "10.222.30.0/24"]
   subnets_map_public_ip_on_launch = false
-  subnets_route_tables            = ["${module.env_test-1.private_nats_ids}"]
-  vpc_id                          = "${module.vpc_test-1.vpc_id}"
+  additional_security_group_ids   = ["sg-XXXXXXXX"]     # manual entry
+  subnets_route_tables            = ["${var.private_nat_route_table_ids}"]
+  sg_cb_ingress_rule {
+    cidr_blocks = "172.16.100.0/24,172.16.101.0/24"
+    protocol    = "tcp"
+    from_port   = 80
+    to_port     = 80
+  }
+  sg_cb_ingress_rule {
+    cidr_blocks = "172.16.100.0/24,172.16.101.0/24"
+    protocol    = "tcp"
+    from_port   = 443
+    to_port     = 443
+  }
+  sg_ssg_ingress_rule {
+    source_sg_id = "sg-XXXXXXXX"          # manual entry
+    protocol     = "tcp"
+    from_port    = 987
+    to_port      = 987
+  }
+  sg_pl_cb_egress_rule {
+    cidr_blocks     = "172.16.100.0/24,172.16.101.0/24"
+    prefix_list_ids = "pl-XXXXXXXX"       # manual entry
+    protocol        = "tcp"
+    from_port       = 8080
+    to_port         = 8080
+  }
+  sg_pl_cb_egress_rule {
+    cidr_blocks     = ""
+    prefix_list_ids = "pl-XXXXXXXX"       # manual entry
+    protocol        = "tcp"
+    from_port       = 443
+    to_port         = 443
+  }
+  sg_pl_ssg_egress_rule {
+    source_sg_id    = "sg-XXXXXXXX"       # manual entry
+    prefix_list_ids = "pl-XXXXXXXX"       # manual entry
+    protocol        = "tcp"
+    from_port       = 123
+    to_port         = 123
+  }
+  sg_pl_ssg_egress_rule {
+    source_sg_id    = ""
+    prefix_list_ids = "pl-XXXXXXXX"       # manual entry
+    protocol        = "tcp"
+    from_port       = 456
+    to_port         = 456
+  }
   tags {
-    Environment = "${module.vpc_test-1.environment}"
-    Application = "${module.vpc_test-1.project}"
+    Environment = "TESTENV"
+    Application = "TESTPROJ"
     Tier        = "PRI"
   }
 }
@@ -22,5 +77,32 @@ module "mservice_test-1" {
 resource "aws_route_table_association" "test-1_private-nat" {
   count          = "3"
   subnet_id      = "${module.mservice_test-1.subnet_ids[count.index]}"
-  route_table_id = "${element(module.env_test-1.private_nats_ids, count.index)}"
+  route_table_id = "${element(var.private_nat_route_table_ids, count.index)}"
 }
+
+# module "mservice_test-1" {
+#   source                          = "../../terraform-microservice"
+#   additional_security_group_ids   = ["${module.env_test-1.core_sg_id}"]
+#   asg_size_max                    = "3"
+#   asg_size_min                    = "3"
+#   availability_zones              = "${module.env_test-1.azs_available_names}"
+#   aws_region                      = "${module.env_test-1.aws_region}"
+#   lc_ami_id                       = "ami-edb9069e"
+#   lc_instance_type                = "t2.micro"
+#   name                            = "test1"
+#   subnets_cidr                    = ["10.222.10.0/24", "10.222.20.0/24", "10.222.30.0/24"]
+#   subnets_map_public_ip_on_launch = false
+#   subnets_route_tables            = ["${module.env_test-1.private_nats_ids}"]
+#   vpc_id                          = "${module.vpc_test-1.vpc_id}"
+#   tags {
+#     Environment = "${module.vpc_test-1.environment}"
+#     Application = "${module.vpc_test-1.project}"
+#     Tier        = "PRI"
+#   }
+# }
+
+# resource "aws_route_table_association" "test-1_private-nat" {
+#   count          = "3"
+#   subnet_id      = "${module.mservice_test-1.subnet_ids[count.index]}"
+#   route_table_id = "${element(module.env_test-1.private_nats_ids, count.index)}"
+# }
